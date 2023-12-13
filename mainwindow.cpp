f6c84fccd0e80cff58e75ee2f6482c306910fdc8
Ульяна Атанова <ula122002@gmail.com>
2023-12-14 04:08:43 +0700

    feat: add base functionality

---
+++ mainwindow.cpp
@@ -0,0 +1,120 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->graph->setInteraction(QCP::iRangeZoom, true);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_radioButton_1_clicked()
+{
+    dt = 0.5;
+    return;
+}
+
+void MainWindow::on_radioButton_2_clicked()
+{
+    if (true) { dt = 0.2; };
+    return;
+}
+
+
+void MainWindow::on_radioButton_3_clicked()
+{
+    if (true) { dt = 0.1; };
+    return;
+}
+
+
+void MainWindow::on_radioButton_4_clicked()
+{
+    if (true) { dt = 0.01; };
+    return;
+}
+
+
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    //Button Start
+    //Enter values
+
+    a = ui->numAngle->value();
+    v = ui->numSpeed->value();
+    S = ui->numSize->value();
+    m = ui->numWeight->value();
+    y = ui->numHeight->value();
+    x = 0;
+
+    beta = 0.5 * C * S * rho;
+    k = beta / m;
+    cosa = cos(a * PI / 180);
+    sina = sin(a * PI / 180);
+    vx = v * cosa;
+    vy = v * sina;
+
+    //Check if dx is not marked
+
+    if (dt == 0)
+    {
+        QMessageBox::critical(this, "Message","You did not set accuracy!");
+    }
+    else
+    {
+        QList<double> X;
+        QList<double> Y;
+        while (y>=0)
+        {
+            //Calculations
+            root = sqrt(vx * vx + vy * vy);
+            vx = vx - k * vx * root * dt;
+            vy = vy - (g + k * vy * root) * dt;
+
+            x = x + vx * dt;
+            X.append(x);
+            y = y + vy * dt;
+            Y.append(y);
+
+            //Find max height
+
+            if (y_max < y)
+            {
+                y_max = y;
+            }
+        }
+
+        int n = X.size();
+        QVector<double> Xs(n);
+        QVector<double> Ys(n);
+        Xs = X.toVector();
+        Ys = Y.toVector();
+
+        // создаем график и добавляем данные:
+        ui->graph->addGraph();
+        ui->graph->graph(0)->setData(Xs,Ys);
+        // задаем имена осей координат
+        ui->graph->xAxis->setLabel("Distance");
+        ui->graph->yAxis->setLabel("Height");
+        ui->graph->replot();
+
+        distance = x;
+        end_speed = root;
+    }
+
+}
+
+
+
+
+
+
+
---
+++ mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "qcustomplot.h"
+#include <QMainWindow>
+#include <QDebug>
+#define PI 3.14159265
+
+namespace Ui {
+class MainWindow;
+}
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_1_clicked();
+
+    void on_radioButton_1_clicked();
+
+    void on_radioButton_2_clicked();
+
+    void on_radioButton_3_clicked();
+
+    void on_radioButton_4_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    QCustomPlot *customPlot;      // Объявляем графическое полотно
+    QCPGraph *graph;             // Объявляем график
+
+
+private: double a, v, S, m, vx, vy, cosa, sina, beta, k, x, y, root;
+private: float dt = 0;
+
+private: double distance, end_speed;
+    const float rho = 1.29;
+    const float C = 0.15;
+    const float g = 9.81;
+
+private: double y_max = 0;
+};
+#endif // MAINWINDOW_H
